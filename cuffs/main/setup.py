5867b896984649c9a5a27f7360f8b850b02043d9
Dirk van den Bekerom <dcmvdbekerom@gmail.com>
2020-07-06 10:10:54 -0400

    Update setup.py

--- cuffs/main/setup.py
+++ cuffs/main/setup.py
@@ -1,29 +1,9 @@
-##from setuptools import setup
-##from Cython.Build import cythonize
-##
-##setup(
-##    ext_modules = cythonize("py_cuffs.pyx")
-##)
+from setuptools import setup
+from Cython.Build import cythonize
+import numpy as np
 
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-import os
-import sys
-from pathlib import Path
+setup(
+    ext_modules = cythonize("py_cuffs.pyx"),
+    include_dirs = [np.get_include()]
+)
 
-pypath = Path(os.path.dirname(sys.executable))
-includepath = Path(pypath.as_posix()+'/Lib/site-packages/numpy/core/include')
-print(includepath)
-
-ext_modules = [Extension('py_cuffs',
-                   sources=['py_cuffs.pyx'],
-                   include_dirs=[includepath],
-                   language='c++',
-##                   extra_compile_args=['/openmp',
-##                       '/O2', '/favor:INTEL64', '/fp:fast'],
-                   extra_link_args=[],
-                   )]
-setup(name = 'py_cuffs',
-      cmdclass = {'build_ext': build_ext},
-      ext_modules = ext_modules)
